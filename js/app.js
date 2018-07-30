6e5976cf872604dcf709de2c2eef2db21309f37d
jeremybowden73 <jeremybowden73.gmail.com>
2018-07-30 23:11:27 +0100

    Updated Twit methods for tweet lookup

--- js/app.js
+++ js/app.js
@@ -5,34 +5,22 @@ const accessKeys = require('./config');
 const Twit = require('twit');
 const T = new Twit(accessKeys);
 
-// T.get('search/tweets', { q: 'vardy since:2011-07-11', count: 5 }, function (err, data, response) {
-//   console.log(data)
-// });
-
 // required data from the Twitter API
 // 1. Most recent tweets (x 5)
-
-// 2. Most recent friends (users that you follow) (x5)
-// 2978109514
-T.get('friends/ids', { screen_name: 'theJeremyBowden', count: 8 }, function (err, data, response) {
-  // data is a JSON object containing the key "ids"; the value of "ids" is an array
-  // containing the user_ids for the user's friends
-  const friendsIDs = data.ids;
-  console.log(friendsIDs);
-
-  T.get('users/lookup', { user_id: friendsIDs }, function (err, data, response) {
-    // data is an array of JSON objects, one for each of the comma-separated list in user_id
-    const friendsObj = data;
-    // console.log(friendsObj);
-    // console.log(friendsObj[1].name);
-    friendsObj.forEach(element => {
-      console.log(element.name);
-    });
-  });
-
-
+T.get('users/lookup', { screen_name: 'theJeremyBowden' }, function (err, data, response) {
+  console.log(data);
 });
 
+
+// 2. Most recent friends (users that you follow) (x5)
+T.get('friends/list', { screen_name: 'theJeremyBowden', count: 5 }, function (err, data, response) {
+  // data is a JSON object with one property, "users"
+  // value of data.users is an array of JSON objects, one for each of the users that the account is following
+  const users = data.users;
+  users.forEach(element => {
+    // console.log(element.name);
+  });
+});
 
 
 
